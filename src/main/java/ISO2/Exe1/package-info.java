05fa2bb43de9785a133a47c9cd78d2b1af512806
Javier López Cabrera <javier.lopez76@alu.uclm.es>
2025-12-17 13:16:34 +0100

    chore(maintenance): add package-info for iso2.exe1 packages to satisfy Checkstyle

---
+++ src/main/java/ISO2/Exe1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package-level documentation for the iso2.exe1 package.
+ *
+ * This package contains the main application entry point and related classes
+ * for the Exe1 exercise used in the course.
+ *
+ * Generated on 2025-12-17 as part of maintenance work.
+ */
+package iso2.exe1;
